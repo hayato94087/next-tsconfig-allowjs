d360a0cc6fa5e8f7e26420e527249f74caf52010
hayato94087 <hayato94087@gmail.com>
2024-03-27 07:53:55 +0900

    feat:allowJsを無効化しエラーを確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { type FC } from "react";
+import { message } from "@/lib";
 
 const Home: FC = () => {
+  console.log(message);
   return (
     <div className="">
       <div className="text-lg font-bold">Home</div>
